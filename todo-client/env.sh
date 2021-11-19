4b74eceefcc0ebabbfb83115724f8c8794b0e54c
Hasan Gürsoy <hasan@robomotion.io>
2021-11-20 02:17:05 +0300

    env.sh fix

--- todo-client/env.sh
+++ todo-client/env.sh
@@ -4,5 +4,5 @@ rm -rf ./todo/env-config.js
 touch ./todo/env-config.js
 
 echo "window.env = {" >> ./todo/env-config.js
-echo "  API_URL:'${API_URL}'" >> ./todo/env-config.js
+echo "  REACT_APP_API_URL:'${API_URL}'" >> ./todo/env-config.js
 echo "}" >> ./todo/env-config.js
