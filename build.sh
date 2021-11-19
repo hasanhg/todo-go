8d49d9af48e8c4ecd1348ceea9e1c97e08aaf373
Hasan Gürsoy <65349721+hasanhg@users.noreply.github.com>
2021-11-20 01:29:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-cd todo-client
-npm install
-npm run test
-npm run test:pact
-cd ../todo-api
-go test -v -run TestProvider
+#cd todo-client
+#npm install
+#npm run test
+#npm run test:pact
+#cd ../todo-api
+#go test -v -run TestProvider
+
+cd todo-api
+make
