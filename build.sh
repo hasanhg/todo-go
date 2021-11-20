5e0f392824ce389cf68ae64a17da1e219744f5e9
Hasan Gürsoy <hasan@robomotion.io>
2021-11-20 03:01:40 +0300

    all in one

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
+
 cd todo-client
 npm install
-#npm run test
-#npm run test:pact
+npm run test
+npm run test:pact
 make
 
 cd ../todo-api
-#go test -v -run TestProvider
+go test -v -run TestProvider
 make
